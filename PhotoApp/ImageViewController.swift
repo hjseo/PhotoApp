bf7c87438b2d5b45c99dda21d10cc46f1547d56f
hyejeong seo <seohj92@podbbang.com>
2018-06-29 16:55:36 +0900

    프로젝트 생성

---
+++ PhotoApp/ImageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageViewController.swift
+//  PhotoApp
+//
+//  Created by hyejeong seo on 2018. 6. 29..
+//  Copyright © 2018년 hyejeong seo. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
